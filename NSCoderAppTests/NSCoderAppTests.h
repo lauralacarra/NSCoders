ec234163a8970af63bd8e1fa278db05dc3d8dd6f
veladan <dani_vela@me.com>
2013-02-18 12:23:22 +0100

    First commit

---
+++ NSCoderAppTests/NSCoderAppTests.h
@@ -0,0 +1,13 @@
+//
+//  NSCoderAppTests.h
+//  NSCoderAppTests
+//
+//  Created by Daniel Vela on 18/02/13.
+//  Copyright (c) 2013 nscoder_zgz. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSCoderAppTests : SenTestCase
+
+@end
---
+++ NSCoderAppTests/NSCoderAppTests.m
@@ -0,0 +1,32 @@
+//
+//  NSCoderAppTests.m
+//  NSCoderAppTests
+//
+//  Created by Daniel Vela on 18/02/13.
+//  Copyright (c) 2013 nscoder_zgz. All rights reserved.
+//
+
+#import "NSCoderAppTests.h"
+
+@implementation NSCoderAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NSCoderAppTests");
+}
+
+@end
